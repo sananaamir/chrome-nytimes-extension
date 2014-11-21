4c211ded87cdb7ee905a06606359a5a963eba502
Jokii19 <sananaamir@gmail.com>
2014-11-21 12:49:36 -0600

    Added images to the snippets

--- popup.js
+++ popup.js
@@ -15,14 +15,30 @@ function getArticles(){
     
     //Parsing it into JSON to make it easy to use
     var data = JSON.parse(xmlHttp.responseText);
+    var imgString;
+    var multimediaArrayLength;
 
+    
     //For loop that adds the snippets and readmore links to the list
     for(var i = 0; i < data.response.docs.length; i++){
-        document.getElementById('test').innerHTML += '<li><p>'  + 
+        
+        multimediaArrayLength = data.response.docs[i].multimedia.length;
+        console.log(multimediaArrayLength);
+        if(multimediaArrayLength == 3){
+            imgString = '<img src="http://static01.nyt.com/' + data.response.docs[i].multimedia[2].url + '">';
+        }
+        else if(multimediaArrayLength == 2){
+            imgString = '<img src="http://static01.nyt.com/' + data.response.docs[i].multimedia[1].url + '">';
+        }
+        else{
+            imgString = '';
+        }
+        
+        document.getElementById('test').innerHTML += '<li>' + imgString + '<p>'  + 
             data.response.docs[i].snippet + 
             '</p> <a target="_blank" href="' +                
             data.response.docs[i].web_url + 
-            '">read more</a>' +'</li>';
+            '">read more</a>' +'</li><hr><br>';
     }
     
 };
@@ -31,3 +47,7 @@ function getArticles(){
 document.addEventListener('DOMContentLoaded', function () {
     getArticles();
 });
+
+
+
+//http://static01.nyt.com/images/2014/11/26/dining/26POUR/26POUR-master180.jpg
