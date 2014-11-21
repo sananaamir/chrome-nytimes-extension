d9642e37798b8997aeaba28c955ce3fd7eb87d10
Jokii19 <sananaamir@gmail.com>
2014-11-21 12:53:07 -0600

    Added some comments and made version change

--- popup.js
+++ popup.js
@@ -22,8 +22,12 @@ function getArticles(){
     //For loop that adds the snippets and readmore links to the list
     for(var i = 0; i < data.response.docs.length; i++){
         
+        /*
+        Getting the length of the multimedia array since different
+        articles have different images. Some don'e event have them
+        */
         multimediaArrayLength = data.response.docs[i].multimedia.length;
-        console.log(multimediaArrayLength);
+        
         if(multimediaArrayLength == 3){
             imgString = '<img src="http://static01.nyt.com/' + data.response.docs[i].multimedia[2].url + '">';
         }
@@ -34,6 +38,7 @@ function getArticles(){
             imgString = '';
         }
         
+        //Appended imgString in the list
         document.getElementById('test').innerHTML += '<li>' + imgString + '<p>'  + 
             data.response.docs[i].snippet + 
             '</p> <a target="_blank" href="' +                
