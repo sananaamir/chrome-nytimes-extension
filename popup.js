891b4445aa23f598b47e1ae2534dd5b28a9fd7d2
Jokii19 <sananaamir@gmail.com>
2014-11-20 23:59:37 -0600

    Added files and code for basic display of articles

---
+++ popup.js
@@ -0,0 +1,33 @@
+/*
+Get articles function that handles the get request to the API
+The function gets the json response and injects it into an unordered list
+*/
+function getArticles(){
+    
+    //URL for the nytimes api
+    var URL = "http://api.nytimes.com/svc/search/v2/articlesearch.json?&api-key=adf40b3b4306272aaa839500a3e15cc5:13:70127702";
+
+    //The get request
+    var xmlHttp = null;
+    xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("GET", URL, false);
+    xmlHttp.send();
+    
+    //Parsing it into JSON to make it easy to use
+    var data = JSON.parse(xmlHttp.responseText);
+
+    //For loop that adds the snippets and readmore links to the list
+    for(var i = 0; i < data.response.docs.length; i++){
+        document.getElementById('test').innerHTML += '<li><p>'  + 
+            data.response.docs[i].snippet + 
+            '</p> <a target="_blank" href="' +                
+            data.response.docs[i].web_url + 
+            '">read more</a>' +'</li>';
+    }
+    
+};
+
+//The function gets called once the DOM content has been loaded
+document.addEventListener('DOMContentLoaded', function () {
+    getArticles();
+});
